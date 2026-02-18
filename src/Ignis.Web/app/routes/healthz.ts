03bc0c5eb625b75c0ca732a2e3fc5cd631aa76c9
Ole Kristian Losvik <losvik@gmail.com>
2026-02-18 11:08:15 +0100

    Web: Add healthcheck endpoint
    Adds a Healthz endpoint, useful for docker and kubernetes deployments.

---
+++ src/Ignis.Web/app/routes/healthz.ts
@@ -0,0 +1,9 @@
+export function loader() {
+  return new Response("ok", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+      "cache-control": "no-store",
+    },
+  });
+}
