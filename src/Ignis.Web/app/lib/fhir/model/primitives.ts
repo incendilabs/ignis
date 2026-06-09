51394cf0d24fcfed1f8f8df2bc858fd024704677
Ole Kristian Losvik <losvik@gmail.com>
2026-06-09 18:06:40 +0200

    Web: Add models for FHIR base resources
    Add initial FHIR model types for Resource, Meta, Coding and primitives.

---
+++ src/Ignis.Web/app/lib/fhir/model/primitives.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026, Incendi <info@incendi.no>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * Minimal aliases for commonly used FHIR primitive wire types.
+ *
+ * These stay as plain strings for now; validation will be
+ * added later.
+ */
+export type ResourceId = string;
+export type Instant = string;
+export type Uri = string;
+export type Canonical = string;
+export type Code = string;
---
+++ src/Ignis.Web/app/lib/fhir/model/resource.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026, Incendi <info@incendi.no>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import type { Meta } from "./meta";
+import type { Code, ResourceId, Uri } from "./primitives";
+
+/**
+ * Fhir Resource base.
+ *
+ * References:
+ * - R4: https://hl7.org/fhir/R4/resource.html
+ * - R4B: https://hl7.org/fhir/R4B/resource.html
+ * - R5: https://hl7.org/fhir/R5/resource.html
+ *
+ */
+export interface Resource<TResourceType extends string = string> {
+  resourceType?: TResourceType;
+  id?: ResourceId;
+  meta?: Meta;
+  implicitRules?: Uri;
+  language?: Code;
+  [key: string]: unknown;
+}
